ad5a56abacf1bc6a626342964bc9536ea7509f5d
Kevin Arnold <kevarnold02@gmail.com>
2016-04-15 22:40:14 -0400

    keys.js added
    one piece at a time....

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: '6Eud6oC9RVF0TJs4WuNXvcnFf',
+	consumer_secret:'BkjCab5NuC3VCmX7EvDjnTrTUzGNOs4Qy9gR1bWfQAYVNIDyPb',
+	access_token_key: '721035906044596225-M5HYf2hxezcJQESwTHEDFJRAkd0d66D',
+	access_token_secret: 'EesF8mEPQZ9HMSTpcoLVLw7rEsHCb6cpVIPupHWyjTvpZ',
+}
+
